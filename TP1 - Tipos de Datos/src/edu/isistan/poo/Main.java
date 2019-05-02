498923d1bff64c777c1eb5606861409d6b3b39ee
Juan <Juan@DESKTOP-SFH4IC4>
2019-05-02 10:42:47 -0300

    hasta el 14

--- TP1 - Tipos de Datos/src/edu/isistan/poo/Main.java
+++ TP1 - Tipos de Datos/src/edu/isistan/poo/Main.java
@@ -1,5 +1,9 @@
 package edu.isistan.poo;
-	
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
 	public static void ejercicio1(ClaseA var){
 		System.out.println(var.metodo1());
@@ -44,8 +48,19 @@ public class Main {
 
 	}
 	
+		public static void JavaWithSugar(){
+			List<Integer> list = new ArrayList<Integer>(); //Generics
+			for(int i=0;i<10;i++)
+				list.add(Integer.valueOf(i)); //Autoboxing
+			for(Integer i : list) { //For each
+				System.out.println(list.get(i) + 1); //Unboxing
+			}
+		}
+	
 	public static void main(String[] args) {
+		//TP1-Clases
 		//ej1
+		
 		//ejercicio1(new ClaseA()); A es abstracta no se puede instanciar
 		System.out.println("Con B");
 		ejercicio1(new ClaseB()); // Ca metodo 1, Ca metodo 2 llama a Cb metodo 3, Cb metodo 3
@@ -111,7 +126,73 @@ public class Main {
 		System.out.println(v1.equals(v2)); //true
 		
 		//ej5
-		//-129,128, no van a dar true porque 
+		//-129,128, no van a dar true porque el compilador no les asigna una referencia predefinida
+		
+		//ej6
+		//Son wrappers los del tipo Integer, Double, etc...
+		//Esto se puede:
+		ArrayList<Integer> a1 = new ArrayList<Integer>();
+		//Esto no:
+		//ArrayList<int> a2 = new ArrayList<int>();
+		//Autoboxing y unboxing es wrappear y deswrappear
+		
+		//ej7
+		ArrayList<Integer> al = new ArrayList<>();
+		al.add(10);
+		al.add(9);
+		al.add(8);
+		al.add(7);
+		al.add(5);
+		al.add(4);
+		al.add(3);
+		al.add(2);
+		al.add(1);
+		al.add(0);
+
+		al.remove(2);
+		//Qudaria asi: 10 9 7 6 5 4 3 2 1 0 Remueve el 8
+		System.out.println(al);
+		//Esto saca el 2
+		al.remove(Integer.valueOf(2));
+		
+		//ej8
+		JavaWithSugar();
+
+		//ej9
+		//d, 
+		/*
+		Las variables, métodos y constructores que son declarados privados pueden ser solo
+		accedidos por los miembros de la superclase.
+		b. Las variables, métodos y constructores que son declarados protected pueden ser
+		accedidos por cualquiera de las subclases de la superclase.
+		c. Las variables, métodos y constructores que son declarados públicos pueden ser
+		accedidos por cualquier clase.
+		*/
+		
+		//ej10
+		//Las variables de instancia son variables dentro de una clase pero fuera de cualquier método.
+		
+		//ej11
+		//
+		// int [] myArray = {"1", "2", "3"}; //NO, son Strings
+		//int []myArray2 = (5, 8, 2); //NO, va con llaves
+		// int myArray3 [] [] = {4,9,7,0}; //NO, deberían ser columnas enteras
+
+		int myArray4 [] = {4, 3, 7}; //SI
+		int myArray5 [] = new int[] {4,3,7}; //SI
+		int [] myArray6 = new int[] {4,3,7}; //SI
+		
+		//ej12
+		//Como es en cualqueir paquete protected, si fueran necesariamente del
+		//mismo usaría default.
+		
+		//ej13
+		String str = null;
+		System.out.println(str);
+		//La salida es null
+		//El println usa el valueOf, que si un String es null le asigna el valor "null"
+		
+		//ej14 falta
 		
 		
 	}
